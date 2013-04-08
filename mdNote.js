487649e4593706abd786b0c8c1e175305434807a
Ryan Wu <flyworld@gmail.com>
2013-04-08 20:03:15 +0800

    rename

--- mdNote.js
+++ mdNote.js
@@ -23,7 +23,7 @@ passport.deserializeUser(function(obj, done) {
 });
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 5000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
